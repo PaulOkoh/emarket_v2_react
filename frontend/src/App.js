1396034237d96af77bf14611b5aeeb04ae1936b8
Paul Okoh <pauldokoh@gmail.com>
2023-02-22 16:23:14 -0600

    set up for v2

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">emarket</a>
+      </header>
+      <main>
+        List all
+      </main>
+    </div>
+  );
+}
+
+export default App;
